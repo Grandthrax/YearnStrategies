ad0fcb01bf9a86afc0f232ed152dc77d1f0b2cf8
Robb <rc.villanueva.diaz@gmail.com>
2020-10-21 12:57:09 +0200

    Test: withdraw(uint256) method

--- fork_test.js
+++ fork_test.js
@@ -14,6 +14,8 @@ const web3 = new Web3(new Web3("http://127.0.0.1:8545"));
 
 const unlockAddress = "0x7a8edc710ddeadddb0b539de83f3a306a621e823";
 
+const controllerAddress = "0x2be5d998c95de70d9a38b3d78e49751f10f9e88b";
+
 const DAI = new web3.eth.Contract(IERC20.abi, addresses.tokens.dai);
 
 const cDAI = new web3.eth.Contract(CIERC20.abi, addresses.tokens.cDai);
@@ -26,6 +28,7 @@ const Icompt = new web3.eth.Contract(
 );
 
 const AMOUNT_DEPOSIT_WEI = web3.utils.toWei((2000).toString()); // $2,000
+const AMOUNT_WITHDRAW_WEI = web3.utils.toWei((1999).toString()); // $1999
 
 const test = async () => {
   try {
@@ -45,7 +48,7 @@ const test = async () => {
 
     const [gasPrice, gasCost] = await Promise.all([
       web3.eth.getGasPrice(),
-      tx.estimateGas({ from: unlockAddress }),
+      tx_deposit.estimateGas({ from: unlockAddress }),
     ]);
 
     const data = tx_deposit.encodeABI();
@@ -79,18 +82,41 @@ const test = async () => {
 
     // -- Test withdraw(uint256) method --
     const tx_withdraw = await StrategyContract.methods.withdraw(
-      AMOUNT_DEPOSIT_WEI
+      AMOUNT_WITHDRAW_WEI
     );
+
+    const gasPrice_withdraw = await tx_withdraw.estimateGas({
+      from: unlockAddress,
+    });
 
     const data_withdraw = tx_withdraw.encodeABI();
 
-    const txData = {
+    const txData_withdraw = {
       from: unlockAddress,
       to: StrategyContract.options.address,
       data_withdraw,
       gas: gasCost,
-      gasPrice,
+      gasPrice: gasPrice_withdraw,
     };
+
+    const receipt_withdraw = await web3.eth.sendTransaction(txData_withdraw);
+
+    console.log(`Transaction hash: ${receipt_withdraw.transactionHash}`);
+
+    const AFTER_WITHDRAW = {
+      account_borrowable: await Icompt.methods
+        .getAccountLiquidity(StrategyContract.options.address)
+        .call(),
+      dai_contract_bal: await DAI.methods
+        .balanceOf(StrategyContract.options.address)
+        .call(),
+      cdai_contract_bal: await cDAI.methods
+        .balanceOf(StrategyContract.options.address)
+        .call(),
+      current_pos: await StrategyContract.methods.getCurrentPosition().call(),
+    };
+
+    console.log(AFTER_WITHDRAW);
   } catch (error) {
     console.log(error);
   }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,6 @@ module.exports = function (deployer) {
   deployer.deploy(
     YearnCompDaiStrat,
     "0x2be5d998c95de70d9a38b3d78e49751f10f9e88b"
+    // "0x7a8edc710ddeadddb0b539de83f3a306a621e823" --> Use following address to mockup controller as it has good balace of ETH & DAI
   );
 };
